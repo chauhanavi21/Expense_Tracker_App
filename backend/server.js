ce300b621a968c628ace9a6e7dc1e4746acc2b55
avi212003 <chauhanavi212003@gmail.com>
2026-01-21 16:30:14 -0500

    database setup Transcation Table

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { neon } from "@neondatabase/serverless";
 
 dotenv.config();
 
@@ -12,6 +13,28 @@ app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
 
-app.listen(PORT, () => {
+const sql = neon(process.env.DATABASE_URL);
+
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
     console.log("Server is up and running on PORT:", PORT);
   });
+});
