d839e86f63fa6512644bacbdfd702d006e08fc28
avi212003 <chauhanavi212003@gmail.com>
2026-01-21 18:05:33 -0500

    Transcation id by user

---
+++ backend/controllers/transactionsController.js
@@ -0,0 +1,16 @@
+import { sql } from "../config/db.js";
+
+export async function getTransactionsByUserId(req, res) {
+  try {
+    const { userId } = req.params;
+
+    const transactions = await sql`
+        SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
+      `;
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.log("Error getting the transactions", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
