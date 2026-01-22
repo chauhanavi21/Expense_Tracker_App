58546b520e9b419caea739d4b0a8e69c8733f3dc
avi212003 <chauhanavi212003@gmail.com>
2026-01-21 21:44:41 -0500

    server side update for rate limiter

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import { initDB, sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+import transactionsRoute from "./routes/transactionsRoute.js";
 
 dotenv.config();
 
 const app = express();
 
-
+app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
-app.post("/api/transactions", async (req, res) => {
-  try {
-    const { title, amount, category, user_id } = req.body;
-
-    if (!title || !user_id || !category || amount === undefined) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
-    const transaction = await sql`
-        INSERT INTO transactions(user_id,title,amount,category)
-        VALUES (${user_id},${title},${amount},${category})
-        RETURNING *
-      `;
-
-    console.log(transaction);
-    res.status(201).json(transaction[0]);
-  } catch (error) {
-    console.log("Error creating the transaction", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-
+app.use("/api/transactions", transactionsRoute);
 
 initDB().then(() => {
   app.listen(PORT, () => {
