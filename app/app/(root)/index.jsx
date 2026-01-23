c1de3f755ea63d68a77fca1a0aae574246d2129c
avi212003 <chauhanavi212003@gmail.com>
2026-01-23 15:45:14 -0500

    final changes

--- app/app/(root)/index.jsx
+++ app/app/(root)/index.jsx
@@ -2,12 +2,14 @@ import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 import { Link, useRouter } from "expo-router";
 import { Alert, FlatList, Image, RefreshControl, Text, TouchableOpacity, View } from "react-native";
 import { SignOutButton } from "@/components/SignOutButton";
-
+import { useTransactions } from "../../hooks/useTransactions";
 import { useEffect, useState } from "react";
 import PageLoader from "../../components/PageLoader";
 import { styles } from "../../assets/styles/home.styles";
 import { Ionicons } from "@expo/vector-icons";
-
+import { BalanceCard } from "../../components/BalanceCard";
+import { TransactionItem } from "../../components/TransactionItem";
+import NoTransactionsFound from "../../components/NoTransactionsFound";
 
 export default function Page() {
   const { user } = useUser();
@@ -40,7 +42,9 @@ export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
+        {/* HEADER */}
         <View style={styles.header}>
+          {/* LEFT */}
           <View style={styles.headerLeft}>
             <Image
               source={require("../../assets/images/logo.png")}
@@ -54,6 +58,7 @@ export default function Page() {
               </Text>
             </View>
           </View>
+          {/* RIGHT */}
           <View style={styles.headerRight}>
             <TouchableOpacity style={styles.addButton} onPress={() => router.push("/create")}>
               <Ionicons name="add" size={20} color="#FFF" />
@@ -63,7 +68,24 @@ export default function Page() {
           </View>
         </View>
 
-        
+        <BalanceCard summary={summary} />
+
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+
+      {/* FlatList is a performant way to render long lists in React Native. */}
+      {/* it renders items lazily — only those on the screen. */}
+      <FlatList
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+        renderItem={({ item }) => <TransactionItem item={item} onDelete={handleDelete} />}
+        ListEmptyComponent={<NoTransactionsFound />}
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      />
     </View>
   );
 }
